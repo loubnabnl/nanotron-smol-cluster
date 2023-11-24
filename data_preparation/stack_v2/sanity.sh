c2070a207843a44837ab60f6d27c079186414f7e
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2023-11-24 17:57:44 +0100

    Update sanity.sh

--- data_preparation/stack_v2/sanity.sh
+++ data_preparation/stack_v2/sanity.sh
@@ -1,6 +1,7 @@
 
+# check first 40 tokenized shards for code if their have reasonable size
 for i in $(seq 0 39); do
-    data=/fsx/loubna/data/stack_v2_smol_all_jsonl/
+    data=/fsx/bigcode/bigcode-training/tokenized_stack_v2_final/
     # make sure folder not empty and print ok and show size
     if [ "$(ls -A $data\code_$i)" ]; then
         echo "code_$i ok!"
@@ -11,12 +12,12 @@ for i in $(seq 0 39); do
 
 done
 
-
+# find those that failed and restart you might need to shard them further or rebuild the jsonl file it something's off
 missing=(8 9 23 32 37)
 for i in "${missing[@]}"; do
     echo "Shard: code\_$i"
-    rm -rf /fsx/bigcode/bigcode-training/tokenized_stackv2_smol/$prefix\_$i
-    sbatch -J $i-code-tokenize /fsx/loubna/bigcode_2/code/megatron-smol-cluster/data_preparation/stack_v2/tokenize_stackv2.slurm code\_$i
+    rm -rf /fsx/bigcode/bigcode-training/tokenized_stack_v2_final/$prefix\_$i
+    sbatch -J $i-code-tokenize /fsx/loubna/bigcode_2/code/megatron-smol-cluster/data_preparation/stack_v2/slurm_files/tokenize_stackv2.slurm code\_$i
 done
 
 sbatch -J 9-1-tokenize /fsx/loubna/bigcode_2/code/megatron-smol-cluster/data_preparation/stack_v2/tokenize_stackv2.slurm code_9_1
