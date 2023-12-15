21251d1e89a6003bba96c51d359939793052c745
loubnabnl <loubnabenallal1999@gmail.com>
2023-12-15 16:11:26 +0000

    add eval

---
+++ evaluation/README.md
@@ -0,0 +1,24 @@
+## HumanEval
+
+Given HumanEval generations, we want to run post-processing to remove extra code and then execute them using `bigcode-evaluation-harness` to compute pass@1:
+
+```bash
+# to clean the generations and save them in bigcode-eval-harness format
+# update paths in the python script
+python /fsx/loubna/ckpts/clean_solutions.py --load_path 7b_513b_tokens.json --save_path 7b_513b_tokens.json
+You can run execution with slurm using:
+```
+sbatch /fsx/loubna/ckpts/slurm_eval_nvidia.slurm  7b_513b_tokens
+```
+Or just run this command on a cpu machine (given you have 20 geerations per problem):
+
+```python
+python /fsx/loubna/bigcode_2/code/bigcode-evaluation-harness/main.py \
+    --model $model_name \
+    --tasks humaneval \
+    --n_samples 20 \
+    --allow_code_execution \
+    --use_auth_token \
+    --load_generations_path /fsx/loubna/bigcode_2/code/bigcode-evaluation-harness/nvidia/$load_path.json \
+    --metric_output_path $save_path/metric_$load_path.json \
+```
---
+++ evaluation/clean_solutions.py
@@ -0,0 +1,82 @@
+import json
+import argparse
+from datasets import load_dataset
+
+
+LOAD_DATA_PATH = "/fsx/loubna/ckpts"
+SAVE_DATA_PATH = "/fsx/loubna/bigcode_2/code/bigcode-evaluation-harness/nvidia"
+STOP_TOKENS = ["\nclass", "\ndef", "\n#", "\n@", "\nprint", "\nif", "\n```", "<filename>", "<file_sep>", "<|endoftext|>"]
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--load_path", type=str, default="3b_20samples.json")
+    parser.add_argument("--save_path", type=str, default="generations_3b.json")
+    return parser.parse_args()
+
+
+def stop_at_stop_token(decoded_string, stop_tokens):
+    """
+    Produces the prefix of decoded_string that ends at the first occurrence of
+    a stop_token.
+    WARNING: the decoded_string *must not* include the prompt, which may have stop tokens
+    itself.
+    """
+    min_stop_index = len(decoded_string)
+    for stop_token in stop_tokens:
+        stop_index = decoded_string.find(stop_token)
+        if stop_index != -1 and stop_index < min_stop_index:
+            min_stop_index = stop_index
+    return decoded_string[:min_stop_index]
+
+
+def postprocess_generation(generation, prompt):
+    """Defines the postprocessing for a LM generation.
+    :param generation: str
+        code generation from LM
+    :param idx: int
+        (not used for Humaneval-Task)
+    """
+    if not generation.startswith(prompt[:20]):
+        print(f"issue with generation: {generation}")
+        print(f"origin prompt: {prompt}")
+    generation = generation[len(prompt) :]
+    return prompt + stop_at_stop_token(generation, STOP_TOKENS)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    # load HumanEval to get the prompts
+    ds = load_dataset("openai_humaneval", split="test")
+
+    # load generations data
+    data = []
+    with open(f"{LOAD_DATA_PATH}/{args.load_path}", "r") as file:
+        i = 0
+        for line in file:
+            i += 1
+            data.append(json.loads(line))
+
+    # post-process the generations
+    final_generations = []
+    for i, problem in enumerate(data):
+        # prepare each fo teh 164 problems
+        solutions = []
+        prompt = ds[i]["prompt"].strip() 
+        for sol in problem["completion"]:
+            # post-process all 20 solutions
+            clean_sol = postprocess_generation(sol, prompt)
+            solutions.append(clean_sol)
+        final_generations.append(solutions)
+
+    # save new generations
+    with open(f"{SAVE_DATA_PATH}/{args.save_path}", "w") as fp:
+        json.dump(final_generations, fp)
+
+    print(f"Data saved at {SAVE_DATA_PATH}/{args.save_path} 🎉")
+    # sanity checks
+    print(f"example 0 solution 5:\n{final_generations[0][5]}")
+    print("-" * 60)
+    print(f"example 56 solution 13:\n{final_generations[56][13]}")
+    print("-" * 60)
+    print(f"example 102 solution 0:\n{final_generations[102][0]}")
