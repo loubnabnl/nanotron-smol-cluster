ebb2b136cdd524f4421ce8f7c8d50a3b22184542
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2024-02-09 14:43:24 +0100

    Update tokenize.py

--- data_preparation/stack_v2/tools/datatrove/tokenize.py
+++ data_preparation/stack_v2/tools/datatrove/tokenize.py
@@ -1,10 +1,31 @@
+import os
+
 from datatrove.executor.slurm import SlurmPipelineExecutor
 from datatrove.pipeline.readers import JsonlReader
 from datatrove.pipeline.tokens.merger import DocumentTokenizerMerger
 from datatrove.pipeline.tokens.tokenizer import DocumentTokenizer
 
 
-print(f"tokenizing code")
+DATASET_NAME = "stack_v2_train_json"
+
+data_sources = ["pull_requests",
+    "stackoverflow",
+    "owm",
+    "lhq_data",
+    "kaggle_scripts",
+    "jupyter_structured",
+    "jupyter_scripts",
+    "issues",
+]
+data_sources = ["stack_3b"]
+
+N_TASKS = 256
+logs_file = "/fsx/loubna/logs/tokenize_mistral"
+data_path = f"/fsx/bigcode/data/stack_tokenize_mistralr/stack_3b"
+os.makedirs(data_path, exist_ok=True)
+print(f"tokenizing with {N_TASKS} logs at /fsx/loubna/logs/tokenize_mistral")
+print(f"data path is {data_path}")
+
 dist_executor = SlurmPipelineExecutor(
     job_name=f"tokenize",
     pipeline=[
@@ -13,35 +34,43 @@ dist_executor = SlurmPipelineExecutor(
             text_key="content",
         ),
         DocumentTokenizer(
-            output_folder=f"/fsx/bigcode/data/stack_tokenisze_mistral/stack_3b/",
+            output_folder=data_path,
             tokenizer_name="mistralai/Mistral-7B-v0.1",
-            save_filename=f"code_tokenized",
+            save_filename=f"code",
             eos_token="</s>",
         ),
     ],
-    tasks=260,
+    tasks=N_TASKS,
     time="20:00:00",
     partition="hopper-cpu",
-    logging_dir=f"/fsx/loubna/logs/tokenization_cpu/",
-    cpus_per_task=24,
-    #mem_per_cpu_gb=4,
+    logging_dir=logs_file,
+    cpus_per_task=20,
+    mem_per_cpu_gb=4,
 )
 dist_executor.run()
 
-merge_executor = SlurmPipelineExecutor(
-    job_name=f"tokenize2",
-    pipeline=[
-        DocumentTokenizerMerger(
-            input_folder=f"/fsx/bigcode/data/stack_tokenisze_mistral/stack_3b/",
-            output_folder=f"/fsx/bigcode/data/stack_tokenisze_mistral/standard/stack_3b/",
-            save_filename=f"{DATASET_NAME}",
-        ),
-    ],
-    tasks=1,
-    time="50:00:00",
-    partition="hopper-cpu",
-    logging_dir=f"/fsx/loubna/logs/tokenization/tokenize_stack_merged",
-    mem_per_cpu_gb=12,
-    depends=dist_executor,
-)
-merge_executor.run()
+
+# # this works
+# from datatrove.pipeline.readers import JsonlReader
+# from datatrove.pipeline.tokens.tokenizer import DocumentTokenizer
+
+# reader = JsonlReader(
+#                 f"s3://bigcode-datasets-us-east-1/stack_v2_train_json/stack_3b/",
+#                 text_key="content",
+#             )
+
+# tokenizer_pipeline_1 = DocumentTokenizer(
+#              output_folder=f"/fsx/bigcode/data/tests/gpt2",
+#              tokenizer_name="gpt2",
+#              save_filename=f"code_tokenized",
+#          )
+# tokenizer_pipeline_2 = DocumentTokenizer(
+#              output_folder=f"/fsx/bigcode/data/tests/mistral",
+#              tokenizer_name="mistralai/Mistral-7B-v0.1",
+#              save_filename=f"code_tokenized",
+#              eos_token="</s>",
+#          )
+
+# tokenizer_pipeline_2.write_unshuffled(reader(), "test_2")
+
+
