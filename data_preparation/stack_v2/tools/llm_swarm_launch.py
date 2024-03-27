d8f41c6e697b2b2fb139e376282a21dd3b126242
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2024-03-27 12:27:56 +0100

    Update llm_swarm_launch.py

--- data_preparation/stack_v2/tools/llm_swarm_launch.py
+++ data_preparation/stack_v2/tools/llm_swarm_launch.py
@@ -7,7 +7,7 @@ from threading import Thread
 from time import sleep
 
 
-# change these paths and run llm_swarm_launch.py
+# change these paths and run `python llm_swarm_launch.py`
 SLURM_LOGS_FOLDER = "/fsx/loubna/projects/llm-swarm/slurm/logs"
 slurm_path = "/fsx/loubna/projects/llm-swarm/slurm/tgi_1711535789_tgi.slurm"
 number_tgi_instances = 10
